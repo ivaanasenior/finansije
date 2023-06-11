c2c2d35f7985af788ceea95c2b280681019df155
ivaanasenior <101055304+ivaanasenior@users.noreply.github.com>
2023-06-11 19:26:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+var detailsRow = null;
+
+function showBookDetails(row) {
+  clearDetailsRow();
+
+  var table = document.getElementById("myTable");
+  var newRow = table.insertRow(row + 1);
+  newRow.className = "book-details";
+  var newCell = newRow.insertCell(0);
+  newCell.colSpan = "4";
+  newCell.innerHTML = "Detalji o knjizi " + row;
+
+  detailsRow = newRow;
+}
+
+function showCalculatorDetails(row) {
+  clearDetailsRow();
+
+  var table = document.getElementById("myTable");
+  var newRow = table.insertRow(row + 1);
+  newRow.className = "calculator-details";
+  var newCell = newRow.insertCell(0);
+  newCell.colSpan = "4";
+  newCell.innerHTML = "Detalji o kalkulatoru " + row;
+
+  detailsRow = newRow;
+}
+
+function clearDetailsRow() {
+  if (detailsRow !== null) {
+    var table = document.getElementById("myTable");
+    table.deleteRow(detailsRow.rowIndex);
+    detailsRow = null;
+  }
+}
+
+
+
+function openLicnaprimanja() {
+  window.location.href = "licnaprimanja.html";
+}
+
+function closeLicnaprimanja() {
+  window.location.href = "index.html";
+}
+
+function openZakonoradu() {
+  window.location.href = "../Konta/docs/zakon-o-radu.htm";
+}
+
+function closeZakonoradu() {
+  window.location.href = "../index.html";
+}
